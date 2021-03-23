34368622d51e7c65408a1a8031a502bb74bfbb99
hariprasanth23 <harifbi175@gmail.com>
2021-03-23 16:48:18 +0530

    new commit

---
+++ Basic-Databinding/src/app/string-interpolation/string-interpolation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-string-interpolation',
+  templateUrl: './string-interpolation.component.html',
+  styleUrls: ['./string-interpolation.component.css']
+})
+export class StringInterpolationComponent implements OnInit {
+
+  title = 'This is String Interpolation ';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
